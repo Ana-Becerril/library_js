944e2969df74e6c06b98af61250dfde727f009ef
Ana- Becerril <ana.becbel@gmail.com>
2020-08-31 15:24:12 -0500

    conected HTML, CSS and JS files

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let content= document.getElementById("content")
+content.innerHTML =Book
+
+let myLibrary = [];
+//constructor function
+function Book( title, author, pages, status) {
+    this.title=title,
+    this.author=author,
+    this.pages=pages,
+    this.status=read
+}
+
+function addBookToLibrary() {
+  // add stuff here
+}
+
