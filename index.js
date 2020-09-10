8ece154f92b0bc27acb4533c15df958be6650406
Ana- Becerril <ana.becbel@gmail.com>
2020-09-09 21:03:53 -0500

    did a new parent for the form, now the form appear whith anoter display

--- index.js
+++ index.js
@@ -55,7 +55,32 @@ function bookBuilder(title, author, pages, status){
 
 console.log(myLibrary)
 //DOM para observar el nuevo libro en library_store
-document.getElementById("newTitle").innerHTML=newBook.title;
-document.getElementById("newAuthor").innerHTML=newBook.author;
+//document.getElementById("newTitle").innerHTML=newBook.title;
+//document.getElementById("newAuthor").innerHTML=newBook.author;
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
